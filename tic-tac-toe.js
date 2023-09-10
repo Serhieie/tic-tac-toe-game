5fcb14b57be8077a4e9b2e1c96507d4b7ce0a04e
Serhieie <89160714+Serhieie@users.noreply.github.com>
2023-09-10 18:44:54 +0300

    Dead heat update

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -6,10 +6,8 @@ const valueO = document.querySelector('.o-wins-value')
 let valueXTextContent = 0;
 let valueOTextContent = 0;
 let player = "X";
-let result = false;
 let playerXCombo = [];
 let playerOCombo = [];
-const endGame = playerXCombo.length + playerOCombo.length === 9;
 const winnerCombo = [
     [1 ,2, 3],
     [4, 5, 6],
@@ -21,49 +19,6 @@ const winnerCombo = [
     [3, 5, 7],
 ];
 
-content.addEventListener("click", onClick);
-
-function onClick (evt) {
-    const {target} = evt;
-     if(!target.classList.contains("js-item") || target.textContent) {
-        return;
-    }; 
-    
-    const id = Number(target.dataset.id)
-    if (player === "X") {
-        playerXCombo.push(id);
-        result = isWinner(playerXCombo);
-    } else {
-        playerOCombo.push(id)
-        result = isWinner(playerOCombo);
-    }
-
-    target.textContent = player;
-
-    if (result) {
-        winnerMsg.textContent = `THE WINNER IS   > ${player} <`;
-        if (player === "X") {
-            valueX.textContent = valueXTextContent += 1;
-        } else if (player === "O") {
-            valueO.textContent = valueOTextContent += 1;
-        }
-        winnerMsg.classList.add("finded");
-        content.removeEventListener("click", onClick);
-        return;
-    } else if (endGame) {
-        winnerMsg.textContent = `HERE IS DEAD HEAT TRY AGAIN`;
-        winnerMsg.classList.add("finded");
-        content.removeEventListener("click", onClick);
-        return;
-    }
-
-    player = player === "X" ? "O" : "X";
-};
-
-function isWinner (arr) {
-    return winnerCombo.some((item) => item.every(id => arr.includes(id)));
-};
-
 function newGame() {
     let markup = '';
     for (let i = 1; i < 10; i+=1) {
@@ -72,6 +27,51 @@ function newGame() {
     content.innerHTML = markup;
 };
 newGame();
+
+content.addEventListener("click", onClick);
+
+function onClick (evt) {
+    const {target} = evt;
+     if(!target.classList.contains("js-item") || target.textContent) {
+        return;
+    }
+    
+    const id = Number(target.dataset.id)
+    let result = false;
+
+    if (player === "X") {
+        playerXCombo.push(id);
+        result = isWinner(playerXCombo);
+    } else {
+        playerOCombo.push(id)
+        result = isWinner(playerOCombo);
+    }
+    
+    target.textContent = player;
+    const endGame = playerXCombo.length + playerOCombo.length === 9;
+    if (result) {
+        winnerMsg.textContent = `THE WINNER IS   > ${player} <`;
+        if (player === "X") {
+            valueX.textContent = valueXTextContent += 1;
+        } else if (player === "O") {
+            valueO.textContent = valueOTextContent += 1;
+        }
+        winnerMsg.classList.add("finded");
+        winnerMsg.classList.add("finded");
+        content.removeEventListener("click", onClick);
+        return;
+    } else if(endGame) {
+        winnerMsg.textContent = `HERE IS DEAD HEAT TRY AGAIN`;
+        winnerMsg.classList.add("finded");
+        content.removeEventListener("click", onClick);
+        return;
+    }
+    player = player === "X" ? "O" : "X";
+};
+
+function isWinner (arr) {
+    return winnerCombo.some((item) => item.every(id => arr.includes(id)));
+};
 
 function restartGame(evt){
     content.addEventListener("click", onClick);
